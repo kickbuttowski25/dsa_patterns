caf505426fef383bcc14eb4c72e84f7e2c34cad2
Vishal Singh Bisht <vishalsinghbisht25@gmail.com>
2023-12-02 11:50:51 +0530

    Right patterns

--- Patterns_dsa.playground/Contents.swift
+++ Patterns_dsa.playground/Contents.swift
@@ -6,7 +6,7 @@ func pattern1(n: Int, m: Int) {
         for _ in 0..<m {
             print("*", terminator: "")
         }
-    print(separator: " ")
+        print(separator: " ")
     }
     
 }
@@ -14,7 +14,7 @@ func pattern1(n: Int, m: Int) {
 //pattern1(n: 1, m: 8)
 
 //MARK: semi triangular pattern
-func rightPattern(n: Int) {
+func leftPattern(n: Int) {
     for i in  0..<n {
         for _ in 0..<i + 1 {
             print("*", terminator: "")
@@ -23,4 +23,18 @@ func rightPattern(n: Int) {
     }
 }
 
-rightPattern(n: 7)
+//leftPattern(n: 7)
+
+//MARK: right semi triangular pattern
+func rightPattern(n: Int) {
+    for i in  0..<n {
+        for j in 0..<n {
+            if n - (n - j) > i + j {
+                print("*", terminator: " ")
+            }
+        }
+        print(separator: " ")
+    }
+}
+
+rightPattern(n: 5)
