a8fd976af93acb7cd3e4958b99c70e06460701a7
Vishal Singh Bisht <vishalsinghbisht25@gmail.com>
2023-12-02 10:27:42 +0530

    n * m patterns, using for loop

--- Patterns_dsa.playground/Contents.swift
+++ Patterns_dsa.playground/Contents.swift
@@ -1,7 +1,14 @@
 import UIKit
 
-
-func pattern1(n: Int) {
-    
+// MARK: n * m patterns
+func pattern1(n: Int, m: Int) {
+    for i in 0..<n {
+        for j in 0..<m {
+            print("*", terminator: "")
+        }
+    print(separator: " ")
+    }
     
 }
+
+pattern1(n: 2, m: 2)
